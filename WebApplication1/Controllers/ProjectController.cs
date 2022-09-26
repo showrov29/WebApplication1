850d8b273308cfd0b3e29a6ec9c5b3c2fdcc5155
Atif Ahmed <showrovislam29@gmail.com>
2022-09-26 17:41:36 +0600

    mvc

---
+++ WebApplication1/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
